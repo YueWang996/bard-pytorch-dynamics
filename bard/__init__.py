0a862e8f0a47e80b57a9eb547ea64d3d436987b0
justin <yw19y19@soton.ac.uk>
2025-10-08 02:33:11 +0100

    import changed

--- bard/__init__.py
+++ bard/__init__.py
@@ -5,22 +5,26 @@ This library provides efficient, GPU-accelerated implementations of robot
 kinematics and dynamics algorithms with support for batch processing.
 """
 
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version
+    __version__ = version("bard")
+except Exception:
+    __version__ = "unknown"
+
 __author__ = "Yue Wang"
 
 from .core.chain import Chain
-from .core.kinematics import calc_forward_kinematics, end_effector_acceleration
-from .core.jacobian import calc_jacobian
-from .core.dynamics import calc_inverse_dynamics, crba_inertia_matrix
+from .core.kinematics import ForwardKinematics, SpatialAcceleration
+from .core.jacobian import Jacobian
+from .core.dynamics import RNEA, CRBA
 from .parsers.urdf import build_chain_from_urdf
 
 __all__ = [
     "Chain",
-    "calc_forward_kinematics",
-    "calc_jacobian",
-    "end_effector_acceleration",
-    "calc_inverse_dynamics",
-    "crba_inertia_matrix",
+    "ForwardKinematics",
+    "SpatialAcceleration",
+    "Jacobian",
+    "RNEA",
+    "CRBA",
     "build_chain_from_urdf",
-    "build_serial_chain_from_urdf",
 ]
